b0c20709cbe405d608890dcc30d6927117da6996
prodriguezUnal <pjrodriguez@unal.edu.co>
2021-12-22 00:17:23 -0500

    updated class Model, constructor y getters and setters of valorJugada

--- src/myProject/Model.java
+++ src/myProject/Model.java
@@ -1,4 +1,17 @@
 package myProject;
 
 public class Model {
+    private int valorJugada;
+
+    public Model(int valorJugada) {
+        this.valorJugada = valorJugada;
+    }
+
+    public int getValorJugada() {
+        return valorJugada;
+    }
+
+    public void setValorJugada(int valorJugada) {
+        this.valorJugada = valorJugada;
+    }
 }
